4d902a4600e92e1fb268263b6572ac79acd38e2a
zhaobo <xjsoni@gmail.com>
2023-02-16 09:29:11 +0800

    Limiter

--- bot/bot.go
+++ bot/bot.go
@@ -1,13 +1,16 @@
 package bot
 
 import (
+	"context"
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/sethvargo/go-limiter/memorystore"
 	"log"
 	"strings"
 	"telegram-chatgpt/conf"
 	"telegram-chatgpt/gpt"
 	"telegram-chatgpt/session"
+	"time"
 )
 
 func Start() {
@@ -19,6 +22,16 @@ func Start() {
 	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	limiter, err := memorystore.New(&memorystore.Config{
+		// Number of tokens allowed per interval.
+		Tokens: 1,
+		// Interval until tokens reset.
+		Interval: time.Second * 30,
+	})
+	if err != nil {
+		log.Panic(err)
+	}
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -50,6 +63,17 @@ func Start() {
 				}
 
 				text = strings.ReplaceAll(update.Message.Text, atBotText, "")
+			}
+
+			_, _, rest, ok, _ := limiter.Take(context.Background(), update.Message.From.UserName)
+
+			if !ok {
+				sub := time.UnixMicro(int64(rest / 1000)).Sub(time.Now())
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("🐢(%vs)", int64(sub.Seconds())))
+				msg.ReplyToMessageID = update.Message.MessageID
+
+				bot.Send(msg)
+				continue
 			}
 
 			reply, err := gpt.Completions(update.Message.From.UserName, strings.TrimSpace(text))
